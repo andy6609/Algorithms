447e245a19750579bed042d02e36706dbfce779d
이동윤 <andy6609@idong-yun-ui-Macmini.local>
2025-08-09 17:04:13 +0100

    33

---
+++ codeup-100/031.py
@@ -0,0 +1,9 @@
+# 10진 정수 1개를 input  받고 character 로 프린트 하는 것임 
+# chr 는 정수 -> 문자 값, ord () 는 문자 -> 정수값 
+
+#c = int(input())
+#print(chr(c))
+
+c=input()
+c= int(c)
+print(chr(c))
---
+++ codeup-100/033.py
@@ -0,0 +1,9 @@
+# 입력한 거에 1더해서(즉 다음 문자) 뽑기 
+
+#n = ord(input())
+#print (chr(n+1))
+
+n1 = input()
+n2 = ord(n1) +1 
+s = chr (n2)
+print (s)
